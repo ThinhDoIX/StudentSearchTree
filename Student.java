d6f54c2a9dda812e030e3f072b7b7efcb8137bce
YoruDo <45510642+YoruDo@users.noreply.github.com>
2019-04-10 08:50:25 +0700

    Add Student class

---
+++ Student.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+import java.util.*;
+
+public class Student {
+	
+	public static void main(String[] args) {
+		
+		private Integer id;
+		private String ten;
+		private String ngaysinh;
+		private Double tichluy;
+		private Double tinchi;
+		
+		
+		public Student() {}
+		public Student(Integer id, String ten, String ngaysinh, Double tichluy, Double tinchi) {
+			this.id = id;
+			this.ten = ten;
+			this.ngaysinh = ngaysinh;
+			this.tichluy = tichluy;
+			this.tinchi = tinchi;
+		}
+	}
+}
